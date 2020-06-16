2cfec2bf645a017bb23c35703bddc2998e5eface
Daniel Ortiz Zarandona <daozara@gmail.com>
2020-06-15 20:30:28 -0500

    primera entrega codigo fuente

---
+++ EjemploGithub/EjemploGithub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EjemploGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
